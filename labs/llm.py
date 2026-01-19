28ab7a80e8d1fa723b2b97a1085fea8ab8be8bfd
Tore Erdmann <tore.erdmann@alexanderthamm.com>
2026-01-19 11:55:14 +0100

    update llm

--- labs/llm.py
+++ labs/llm.py
@@ -1,18 +1,17 @@
 import os
 
-from databricks_langchain import ChatDatabricks
-from langchain_openai.chat_models.azure import AzureChatOpenAI
-
 model = None
 
 if "DATABRICKS_RUNTIME_VERSION" in os.environ:
     print("Running on Databricks.")
+    from databricks_langchain import ChatDatabricks
 
     model = ChatDatabricks(endpoint="databricks-claude-sonnet-4-5", temperature=0)
 else:
     print("Running locally.")
 
     import dotenv
+    from langchain_openai.chat_models.azure import AzureChatOpenAI
 
     dotenv.load_dotenv()
     model = AzureChatOpenAI(deployment_name="gpt-4.1", temperature=0)
